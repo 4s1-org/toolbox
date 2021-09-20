3772e8843c22c30542a53369f3c8bd32f47ea38d
Steffen <steffen@4s1.de>
2021-09-20 16:51:47 +0200

    feat: add bootstrap 3+5 colors

---
+++ src/colors/bootstrap5-color.ts
@@ -0,0 +1,58 @@
+/**
+ * Bootstrap5 colors
+ *
+ * Based on https://getbootstrap.com/docs/5.1/customize/color/
+ */
+export abstract class Bootstrap5Color {
+  public static get blue(): string {
+    return '#0d6efd'
+  }
+
+  public static get indigo(): string {
+    return '#6610f2'
+  }
+
+  public static get purple(): string {
+    return '#6f42c1'
+  }
+
+  public static get pink(): string {
+    return '#d63384'
+  }
+
+  public static get red(): string {
+    return '#dc3545'
+  }
+
+  public static get orange(): string {
+    return '#fd7e14'
+  }
+
+  public static get yellow(): string {
+    return '#ffc107'
+  }
+
+  public static get green(): string {
+    return '#198754'
+  }
+
+  public static get teal(): string {
+    return '#20c997'
+  }
+
+  public static get cyan(): string {
+    return '#0dcaf0'
+  }
+
+  public static get gray(): string {
+    return '#adb5bd'
+  }
+
+  public static get white(): string {
+    return '#ffffff'
+  }
+
+  public static get black(): string {
+    return '#000000'
+  }
+}
