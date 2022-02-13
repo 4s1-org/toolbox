de140f7df7b9ec5da9e193ed475309b5ad31bc49
Steffen <steffen@4s1.de>
2022-02-13 11:24:42 +0100

    test: switch from jest to ava

---
+++ ava.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  files: ['src/**/*.spec.ts'],
+  extensions: ['ts'],
+  require: ['ts-node/register'],
+}
--- src/utils.spec.ts
+++ src/utils.spec.ts
@@ -1,88 +1,83 @@
 import { DefaultError } from './default-error'
 import { assertSet, isSet } from './utils'
+import test from 'ava'
 
-describe('Utils', () => {
-  describe('isSet()', () => {
-    it('Value is a string', () => {
-      // Arrange
-      const testVar = 'foobar'
-      // Act
-      const res = isSet(testVar)
-      // Assert
-      expect(res).toEqual(true)
-    })
+test('isSet() - Value is a string', (t) => {
+  // Arrange
+  const testVar = 'foobar'
+  // Act
+  const res = isSet(testVar)
+  // Assert
+  t.is(res, true)
+})
 
-    it('Value is 0', () => {
-      // Arrange
-      const testVar = 0
-      // Act
-      const res = isSet(testVar)
-      // Assert
-      expect(res).toEqual(true)
-    })
+test('isSet() - Value is 0', (t) => {
+  // Arrange
+  const testVar = 0
+  // Act
+  const res = isSet(testVar)
+  // Assert
+  t.is(res, true)
+})
 
-    it('Value is null', () => {
-      // Arrange
-      const testVar = null
-      // Act
-      const res = isSet(testVar)
-      // Assert
-      expect(res).toEqual(false)
-    })
+test('isSet() - Value is null', (t) => {
+  // Arrange
+  const testVar = null
+  // Act
+  const res = isSet(testVar)
+  // Assert
+  t.is(res, false)
+})
 
-    it('Value is undefined', () => {
-      // Arrange
-      const testVar = undefined
-      // Act
-      const res = isSet(testVar)
-      // Assert
-      expect(res).toEqual(false)
-    })
-  })
+test('isSet() - Value is undefined', (t) => {
+  // Arrange
+  const testVar = undefined
+  // Act
+  const res = isSet(testVar)
+  // Assert
+  t.is(res, false)
+})
 
-  describe('assertSet()', () => {
-    it('Value is a string', () => {
-      // Arrange
-      const testVar = 'foobar'
-      // Act
-      const fct = (): void => {
-        assertSet(testVar)
-      }
-      // Assert
-      expect(fct).not.toThrow(DefaultError)
-    })
+test('assertSet() - Value is a string', (t) => {
+  // Arrange
+  const testVar = 'foobar'
+  // Act
+  const fct = (): void => {
+    assertSet(testVar)
+  }
+  // Assert
+  t.notThrows(fct)
+})
 
-    it('Value is 0', () => {
-      // Arrange
-      const testVar = 0
-      // Act
-      const fct = (): void => {
-        assertSet(testVar)
-      }
-      // Assert
-      expect(fct).not.toThrow(DefaultError)
-    })
+test('assertSet() - Value is 0', (t) => {
+  // Arrange
+  const testVar = 0
+  // Act
+  const fct = (): void => {
+    assertSet(testVar)
+  }
+  // Assert
+  t.notThrows(fct)
+})
 
-    it('Value is null', () => {
-      // Arrange
-      const testVar = null
-      // Act
-      const fct = (): void => {
-        assertSet(testVar)
-      }
-      // Assert
-      expect(fct).toThrow(DefaultError)
-    })
+test('assertSet() - Value is null', (t) => {
+  // Arrange
+  const testVar = null
+  // Act
+  const fct = (): void => {
+    assertSet(testVar)
+  }
+  // Assert
+  t.throws(fct, { instanceOf: DefaultError })
+})
 
-    it('Value is undefined', () => {
-      // Arrange
-      const testVar = undefined
-      // Act
-      const fct = (): void => {
-        assertSet(testVar)
-      }
-      // Assert
-      expect(fct).toThrow(DefaultError)
-    })
-  })
+test('assertSet() - Value is undefined', (t) => {
+  // Arrange
+  const testVar = undefined
+  // Act
+  const fct = (): void => {
+    assertSet(testVar)
+  }
+  // Assert
+  t.throws(fct, { instanceOf: DefaultError })
 })
