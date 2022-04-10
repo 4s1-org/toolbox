e69810bfd1e88b99e85e8beaeac1b02948560138
Steffen <steffen@4s1.de>
2022-04-10 20:14:05 +0200

    feat: isSet and assertSet now return NonNullable type
    https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-7.html#assertion-functions

--- src/utils.ts
+++ src/utils.ts
@@ -3,15 +3,15 @@ import { DefaultError } from './default-error'
 /**
  * Check parameter to be unequals undefined and unequals null.
  */
-export function isSet<T>(value: T | undefined | null): value is T {
+export function isSet<T>(value: T | undefined | null): value is NonNullable<T> {
   return value !== undefined && value !== null
 }
 
 /**
  * Throws an error of parameter is undefined or null.
  */
-export function assertSet<T>(value: T | undefined | null): asserts value is T {
-  if (!isSet(value)) {
-    throw new DefaultError('Value is not set.')
+export function assertSet<T>(value: T | undefined | null): asserts value is NonNullable<T> {
+  if (value === undefined || value === null) {
+    throw new DefaultError(`Expected 'value' to be defined, but received ${value}`)
   }
 }
